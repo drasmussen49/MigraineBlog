5e7ae173d17db953a986473e9fe49198c584f734
Dan Rasmussen <drasmussen49@gmail.com>
2020-02-24 16:48:43 -0500

    Created models

---
+++ MigraineBlog/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MigraineBlog.Models
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishDate { get; set; }
+        //public Category Category { get; set; }
+        //public ICollection<Tag> Tag { get; set; }
+
+    }
+}
