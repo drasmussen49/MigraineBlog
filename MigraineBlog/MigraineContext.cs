9e182ef2aad5b2e0e652af55691bf7ffba23d9ee
rhi-teetzel <rhiannateetzel@gmail.com>
2020-02-25 15:56:33 -0500

    added dbcontext layer

---
+++ MigraineBlog/MigraineContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MigraineBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MigraineBlog
+{
+    public class MigraineContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=ArcadeDb;Trusted_Connection=True;";
+            optionsBuilder.UseSqlServer(connectionString)
+                          .UseLazyLoadingProxies();
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        //New Override Method for OnModelCreating to enter seed data
+    }
+}
