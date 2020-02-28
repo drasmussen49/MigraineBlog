f3ed6d9a7f8a6edb9975f81f4538ce0cfe694d69
Dan Rasmussen <drasmussen49@gmail.com>
2020-02-28 15:47:51 -0500

    Finished adding mock tests

--- MigraineBlog.Tests/CategoryControllerTests.cs
+++ MigraineBlog.Tests/CategoryControllerTests.cs
@@ -15,7 +15,7 @@ namespace MigraineBlog.Tests
         CategoryController controller;
         IRepository<Category> categoryMockRepo;
 
-        CategoryControllerTests()
+        public CategoryControllerTests()
         {
             categoryMockRepo = Substitute.For<IRepository<Category>>();
             controller = new CategoryController(categoryMockRepo);
@@ -29,6 +29,43 @@ namespace MigraineBlog.Tests
 
             // Assert
             Assert.IsType<ViewResult>(result);
+        }
+        [Fact]
+        public void Index_Passes_AllCourses_To_View()
+        {
+            // Arrange
+            var expectedCategories = new List<Category>();
+            categoryMockRepo.GetAll().Returns(expectedCategories);
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.Equal(expectedCategories, result.Model);
+        }
+
+        [Fact]
+        public void Details_Returns_A_View()
+        {
+            // Act
+            var result = controller.Details(1);
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Details_Passes_Course_To_View()
+        {
+            // Arrange
+            var expectedCategory = new Category();
+            categoryMockRepo.GetById(1).Returns(expectedCategory);
+
+            // Act
+            var result = controller.Details(1);
+
+            // Assert
+            Assert.Equal(expectedCategory, result.Model);
         }
     }
 }
