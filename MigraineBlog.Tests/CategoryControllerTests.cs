9343303089cc430edb6606c0cb3b3e8760810c0a
Dan Rasmussen <drasmussen49@gmail.com>
2020-02-28 15:37:05 -0500

    Styled CRUD pages more, added NSubstitute and new test class

---
+++ MigraineBlog.Tests/CategoryControllerTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using MigraineBlog.Controllers;
+using MigraineBlog.Models;
+using NSubstitute;
+using MigraineBlog.Repositories;
+
+namespace MigraineBlog.Tests
+{
+    public class CategoryControllerTests
+    {
+
+    }
+}
