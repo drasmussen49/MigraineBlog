cefb078c9034975ac87eccb5690415b5b5adb941
Dan Rasmussen <drasmussen49@gmail.com>
2020-03-01 12:15:20 -0500

    Changed post model to include Tag and TagId, changed create method to add to PostTag table

--- MigraineBlog/Migrations/20200301162748_TestingTagIdInPostModelForSelectTag.Designer.cs
+++ MigraineBlog/Migrations/20200301162748_TestingTagIdInPostModelForSelectTag.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MigraineBlog;
 
 namespace MigraineBlog.Migrations
 {
     [DbContext(typeof(MigraineContext))]
-    partial class MigraineContextModelSnapshot : ModelSnapshot
+    [Migration("20200301162748_TestingTagIdInPostModelForSelectTag")]
+    partial class TestingTagIdInPostModelForSelectTag
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- MigraineBlog/Models/Post.cs
+++ MigraineBlog/Models/Post.cs
@@ -11,10 +11,10 @@ namespace MigraineBlog.Models
         public string Title { get; set; }
         public string Body { get; set; }
         public string Author { get; set; }
-        //public DateTime PublishDate { get; set; }
         public virtual Category Category { get; set; }
         public int CategoryId { get; set; }
-
+        public virtual Tag Tag { get; set; }
+        public int? TagId { get; set; }
         public virtual ICollection<PostTag> PostTags { get; set; }
 
     }
